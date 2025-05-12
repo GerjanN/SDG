f34a80d06c7e4666d78c9101dc5e97076ecc016b
Gerjan <gerjan.nienhuis@gmail.com>
2025-05-12 09:19:03 +0200

    removed private properties

--- database/connect.php
+++ database/connect.php
@@ -2,7 +2,7 @@
 
 function dbConnect() {
   try {
-    $conn = new PDO("mysql:host=localhost;dbname=sdg", "bit_academy", "bit_academy");
+    $conn = new PDO("mysql:host=localhost;dbname=sdg", "DB_NAME", "DB_PASSWORD");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
