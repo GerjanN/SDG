67da05a4dac59b8248006573714ce46e8156d2b5
Gerjan <gerjan.nienhuis@gmail.com>
2025-05-23 14:21:08 +0200

    fixed css and config not working

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$_ENV['DB_HOST'] = 'localhost';
+$_ENV['DB_NAME'] = 'SDG';
+$_ENV['DB_USER'] = 'DB_USERNAME';
+$_ENV['DB_PASS'] = 'DB_PASSWORD';
+
+$_ENV['LANG'] = 'nl';
+$_ENV['TITLE'] = 'SDG';
